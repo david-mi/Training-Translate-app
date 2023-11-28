9f2f25ba5fd62c9b997c1945a88f778884c93f94
David Michel <david.michel2809@gmail.com>
2023-11-28 17:33:04 +0100

    feat: add debounce logic on translation and remove translate button

--- component/translateCard/actions/actions.js
+++ component/translateCard/actions/actions.js
@@ -2,7 +2,6 @@ import styles from "./actions.module.css";
 
 import { createReadAudioButton } from "../../readAudio/readAudio.js";
 import { createCopyButton } from "../../copyText/copyText.js";
-import { translateButton } from "./translate/translate.js";
 
 export const actionsContainer = document.createElement("footer");
 actionsContainer.classList.add(styles.actions);
@@ -13,6 +12,5 @@ const audioReaderButton = createReadAudioButton("translate");
 actionsContainer.append(
   audioReaderButton,
   copyButton,
-  translateButton
 )
 
--- component/translateCard/textEntry/textEntry.js
+++ component/translateCard/textEntry/textEntry.js
@@ -1,6 +1,8 @@
 import styles from "./textEntry.module.css";
 import { globalState } from "../../../globalState.js";
 
+const addTranslatedTextEvent = new Event("addTranslatedText");
+
 export const textEntryElement = document.createElement("div");
 textEntryElement.classList.add(styles.textEntry);
 
@@ -8,16 +10,25 @@ const textArea = document.createElement("textarea");
 textArea.placeholder = "Write your text to translate";
 textArea.maxLength = 500;
 
-function handleBlur(event) {
-  const textToTranslate = event.target.value;
-  globalState.textToTranslate = textToTranslate;
-}
-
-textArea.addEventListener("blur", handleBlur);
-
 function handleInput(event) {
   const textToTranslateLenght = event.target.value.length;
   counterElement.innerText = `${textToTranslateLenght}/500`;
+
+  const textToTranslate = event.target.value;
+  globalState.textToTranslate = textToTranslate;
+
+  console.log("input");
+  debounceInput();
+}
+
+let debounceTimeoutId;
+
+function debounceInput() {
+  clearTimeout(debounceTimeoutId);
+
+  debounceTimeoutId = setTimeout(() => {
+    handleTranslate();
+  }, 500);
 }
 
 textArea.addEventListener("input", handleInput);
@@ -29,3 +40,23 @@ textEntryElement.append(textArea, counterElement);
 
 // focus textArea Element after it has been rendered in the DOM
 setTimeout(() => textArea.focus());
+
+async function handleTranslate() {
+  const apiUrl = new URL("https://api.mymemory.translated.net/get");
+  apiUrl.searchParams.append("q", globalState.textToTranslate);
+  apiUrl.searchParams.append("langpair", `${globalState.chosenLanguageIso}|${globalState.targetLanguageIso}`);
+
+  const response = await fetch(apiUrl.href);
+
+  try {
+    if (response.ok === true) {
+      const data = await response.json();
+
+      const translatedText = data.responseData.translatedText;
+      globalState.translatedText = translatedText;
+      dispatchEvent(addTranslatedTextEvent);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
